4cf131d99e2fe05f70572aed0dcf61b7767f0951
christine <christamsnow@gmail.com>
2020-12-20 16:09:18 -0500

    controller path names updated for comments

--- ZipTube/src/main/java/com/Zipcode/Rocks/Controllers/CommentController.java
+++ ZipTube/src/main/java/com/Zipcode/Rocks/Controllers/CommentController.java
@@ -1,13 +1,11 @@
 package com.Zipcode.Rocks.Controllers;
 
 import com.Zipcode.Rocks.Models.Comment;
-import com.Zipcode.Rocks.Models.User;
 import com.Zipcode.Rocks.Services.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -15,13 +13,13 @@ public class CommentController {
     @Autowired
     CommentService commentService;
 
-    @PostMapping("video/addcomment")
+    @PostMapping("comment/addcomment")
     public ResponseEntity<Comment> addComment(@RequestBody Comment comment){return new ResponseEntity<>(commentService.addComment(comment), HttpStatus.CREATED);}
 
-    @DeleteMapping("delete/{commentID}")
+    @DeleteMapping("deletecomment/{commentID}")
     public void deleteCommentById(@PathVariable Long commentID){commentService.deleteComment(commentID);}
 
-    @GetMapping("comment/{videoID}")
+    @GetMapping("commentsbyvideo/{videoID}")
     public ResponseEntity<List<Comment>> getCommentsByVideoID(@PathVariable Long videoID){return new ResponseEntity<>(commentService.getCommentsByVideo(videoID), HttpStatus.OK);}
 
     @PutMapping("comment/{commentID}")
