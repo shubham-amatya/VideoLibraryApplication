e0373f5bbbdd6fbde6494b00dace2c96a70b4b3f
Jay <73120876+JarrellW@users.noreply.github.com>
2020-12-30 12:48:52 -0500

    Adding Security and Authentication

--- ZipTube/src/main/java/com/Zipcode/Rocks/Models/Video.java
+++ ZipTube/src/main/java/com/Zipcode/Rocks/Models/Video.java
@@ -1,8 +1,5 @@
 package com.Zipcode.Rocks.Models;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.persistence.*;
 import java.util.Date;
 
