024dfe218a91604551f008f0cd17058286d28aa6
christian-p-anderson <christian.p.anderson1@gmail.com>
2021-01-01 13:09:44 -0500

    reformatted code in CommentController

--- ZipTube/src/main/java/com/Zipcode/Rocks/Controllers/CommentController.java
+++ ZipTube/src/main/java/com/Zipcode/Rocks/Controllers/CommentController.java
@@ -14,13 +14,19 @@ public class CommentController {
     CommentService commentService;
 
     @PostMapping("comment/addcomment")
-    public ResponseEntity<Comment> addComment(@RequestBody Comment comment){return new ResponseEntity<>(commentService.addComment(comment), HttpStatus.CREATED);}
+    public ResponseEntity<Comment> addComment(@RequestBody Comment comment){
+        return new ResponseEntity<>(commentService.addComment(comment), HttpStatus.CREATED);
+    }
 
     @DeleteMapping("deletecomment/{commentID}")
-    public void deleteCommentById(@PathVariable Long commentID){commentService.deleteComment(commentID);}
+    public void deleteCommentById(@PathVariable Long commentID){
+        commentService.deleteComment(commentID);
+    }
 
     @GetMapping("commentsbyvideo/{videoID}")
-    public ResponseEntity<List<Comment>> getCommentsByVideoID(@PathVariable Long videoID){return new ResponseEntity<>(commentService.getCommentsByVideo(videoID), HttpStatus.OK);}
+    public ResponseEntity<List<Comment>> getCommentsByVideoID(@PathVariable Long videoID){
+        return new ResponseEntity<>(commentService.getCommentsByVideo(videoID), HttpStatus.OK);
+    }
 
     @PutMapping("comment/{commentID}")
     public ResponseEntity<Comment> updateUser(@RequestBody Comment comment, @PathVariable Long commentID) {
--- ZipTube/src/test/java/com/Zipcode/Rocks/ControllersTests/CommentControllerTest.java
+++ ZipTube/src/test/java/com/Zipcode/Rocks/ControllersTests/CommentControllerTest.java
@@ -2,12 +2,18 @@ package com.Zipcode.Rocks.ControllersTests;
 
 import com.Zipcode.Rocks.Controllers.CommentController;
 import com.Zipcode.Rocks.Models.Comment;
+import com.Zipcode.Rocks.Services.CommentService;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -15,11 +21,21 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.test.web.client.ExpectedCount.times;
+
 @WebMvcTest(controllers = CommentController.class)
 @ActiveProfiles("test")
 public class CommentControllerTest {
-    @Autowired
+
+    @Mock
+    CommentService mock;
+
+   //@Autowired
     private MockMvc mockMvc;
+
+    //No clue what this do//Look up verify on mockito look up mockMVC
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     @MockBean
     private CommentController commentController;
@@ -36,9 +52,7 @@ public class CommentControllerTest {
         this.commentList.add(new Comment("too funny"));
     }
 
-//    @Test
-//    public void shouldAddCommentTest() throws Exception {
-//        Mockito.when(commentController.addComment(new Comment("Love It"))).thenReturn(commentList);
-//        mockMvc.perform();
-//    }
+    @Test
+    public void shouldAddCommentTest() throws Exception {
+    }
 }
