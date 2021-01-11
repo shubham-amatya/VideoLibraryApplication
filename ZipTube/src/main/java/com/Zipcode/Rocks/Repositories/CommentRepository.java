7e90cdb727ddbf666f1d60232779e9493711ddf3
christine <christamsnow@gmail.com>
2021-01-11 16:30:10 -0500

    comments crud repo changed to jpa repo

--- ZipTube/src/main/java/com/Zipcode/Rocks/Repositories/CommentRepository.java
+++ ZipTube/src/main/java/com/Zipcode/Rocks/Repositories/CommentRepository.java
@@ -1,12 +1,12 @@
 package com.Zipcode.Rocks.Repositories;
 
 import com.Zipcode.Rocks.Models.Comment;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CommentRepository extends CrudRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     Comment findCommentByCommentId(Long commentId);
 
