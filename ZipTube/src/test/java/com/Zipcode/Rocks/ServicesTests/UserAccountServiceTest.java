72e429995dae2f8d289c61569f19bd0c44350b62
hillary <hillarygivhan@gmail.com>
2021-01-20 17:10:01 -0500

    service tests for get methods done

--- ZipTube/src/test/java/com/Zipcode/Rocks/ServicesTests/UserAccountServiceTest.java
+++ ZipTube/src/test/java/com/Zipcode/Rocks/ServicesTests/UserAccountServiceTest.java
@@ -3,46 +3,95 @@ package com.Zipcode.Rocks.ServicesTests;
 import com.Zipcode.Rocks.Models.User;
 import com.Zipcode.Rocks.Repositories.UserRepository;
 import com.Zipcode.Rocks.Services.UserService;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.junit4.SpringRunner;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class UserAccountServiceTest {
 
-    @TestConfiguration
-    static class UserServiceTestContextConfiguration{
-        @Bean
-        public UserService userService(){
-            return new UserService();
-        }
-    }
-    @Autowired
-    private UserService userService;
-
-    @MockBean
+    @Mock
     private UserRepository userRepository;
 
-    @Before
-    public void setUp(){
-        User alex = new User("alex1", "alex2", "Alex", "Jones","alexj@aol.com");
-//        Mockito.when(userRepository.findUserByUserName(alex.getUsername()))
-//                .thenReturn(alex);
+    @InjectMocks
+    public UserService userService;
+
+
+    @Test
+    public void findUserByUsernameTest() {
+        //given
+        String username = "alex1";
+        User user = new User("alex1", "1234", "Alex", "Jones", "alexj@aol.com");
+
+        doReturn(user).when(userRepository).findUserByUserName(username);
+
+        //when
+        User actualUser = userService.getUserByUserName(username);
+
+        //Then
+        assertThat(actualUser).isEqualTo(user);
     }
 
     @Test
-    public void findByUsernameTest() {
-        String username = "alex1";
-        User found = userService.getUserByUserName(username);
+    public void findUserById() {
+        //given
+        Long userId = 1L;
+        User user = new User("alex1", "1234", "Alex", "Jones", "alexj@aol.com");
+        user.setUserId(userId);
 
-        assertThat(found.getUsername())
-                .isEqualTo(username);
+        doReturn(user).when(userRepository).findUserByUserId(userId);
+
+        //when
+        User actualUser = userService.getUserByUserId(userId);
+
+        //Then
+        assertThat(actualUser).isEqualTo(user);
+    }
+
+    @Test
+    public void findByUserByEmailTest() {
+        //given
+        String email = "alexj@aol.com";
+        User user = new User("alex1", "1234", "Alex", "Jones", "alexj@aol.com");
+
+        doReturn(user).when(userRepository).findUserByEmail(email);
+
+        //when
+        User actualUser = userService.getUserByEmail(email);
+
+        //Then
+        assertThat(actualUser).isEqualTo(user);
+    }
+
+    @Test
+    public void checkUserNameExistsTest() {
+        User user = new User("alex1", "1234", "Alex", "Jones", "alexj@aol.com");
+        String usernameGiven = "alex1";
+        String response = "DOES NOT EXIST";
+
+        doReturn(null).when(userRepository).findUserByUserName(usernameGiven);
+
+        String expectedResponse = userService.getCheckUserNameExists(usernameGiven);
+
+        assertThat(expectedResponse).isEqualTo(response);
+
+    }
+
+    @Test
+    public void checkEmailExistsTest() {
+        User user = new User("alex1", "1234", "Alex", "Jones", "alexj@aol.com");
+        String emailGiven = "alexj@aol.com";
+
+        doReturn(user).when(userRepository).findUserByUserName(emailGiven);
+
+        String expectedResponse = userService.getCheckUserNameExists(emailGiven);
+
+        assertThat(expectedResponse).isEqualTo(emailGiven);
+
     }
 
 }
